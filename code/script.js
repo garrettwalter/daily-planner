dc9399537e91f33fcea96ecd4fab878e6a36eb7d
Garrett Walter <gtwalter150@gmail.com>
2020-10-04 15:33:11 -0400

    created the hours of the day that display on the left of the application

--- code/script.js
+++ code/script.js
@@ -3,13 +3,27 @@ $(document).ready(function(){
     console.log("Hello World");
 
     // DOM VARS
-
+        var dayBlock = $("#day");
+        var hourBlock = $("#hour");
+        var timeBlock = $("#timeBlock");
+        var textBlock = $("#textBlock");
+        var lockBlock = $("#lockBlock");
     // JS VARS
-
+        var hourOfDay = ["9","10","11","12","1","2","3","4","5"]
     // FUNCTION DEFINITIONS
+        function renderHour (){
+            for (i=0;i<hourOfDay.length;i++){
+                var timeDiv = $("<div>" + hourOfDay[i] + "</div>");
+                timeDiv.attr("class","row");
+                timeDiv.addClass("hourBorder");
+                hourBlock.append(timeDiv);
+                console.log(hourOfDay[i]);
+            }
+            
+        }
     
     // FUNCTION CALLS
-    
+        renderHour();
     // EVENT LISTENERS
 
 })
